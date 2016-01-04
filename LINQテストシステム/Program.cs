b8772659e7172e1739ec5c845c81428c8054f647
ShiranuiNui <daiti506mu@yahoo.co.jp>
2016-01-05 01:43:01 +0900

    初期完成版

---
+++ LINQテストシステム/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQテストシステム
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+            foreach(int x in Number)
+            {
+                Database.Input = x;
+            }
+
+            List<int> ArrayList = Database.Output;
+            var Linq = Number.Where(n => n <5).Select(r => r);
+            foreach(int x in Linq)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+    static class Database
+    {
+        private static System.Collections.Generic.List<int> ArrayList = new List<int>();
+        public static int Input
+        {
+            set
+            {
+                ArrayList.Add(value);
+            }
+        }
+        public static List<int> Output
+        {
+            get
+            {
+                return ArrayList;
+            }
+            
+        }
+
+    }
+}
